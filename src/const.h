c351f7fd10cab3d1ce8525a89dd6d8e9fed46d5a
Zeotix <yuneldy@outlook.com>
2014-08-06 17:04:00 -0300

    Update const.h

--- src/const.h
+++ src/const.h
@@ -522,6 +522,7 @@ enum PlayerFlags : uint64_t {
 #define CHANNEL_ADVERTISINGROOKGAARD 0x06
 #define CHANNEL_HELP 0x07
 #define CHANNEL_GAMEMASTER 0x08
+#define CHANNEL_CAST 0x09
 #define CHANNEL_PRIVATE 0xFFFF
 
 //Reserved player storage key ranges
